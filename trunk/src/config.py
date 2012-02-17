1754d24fba4e42b7879823f3ca7b50b19396ee88
captain-kola@rambler.ru <captain-kola@rambler.ru@e45696f6-423a-daa6-b7a9-a5d8f5f8ab12>
2012-02-17 21:12:54 +0000

    added a LOT of todos, knock yourselves out

--- trunk/src/config.py
+++ trunk/src/config.py
@@ -4,22 +4,21 @@ Created on 19.01.2012
 @author: 2526240
 '''
 
+# ****************** general config ******************
 debug = True
-
-maxBatSize = 15 # maximal bat size in meters
-
-brickSize = (50, 50) # the size of a brick (piece of a block)
-
-period = 0.3 # the time the player have to drag the block before the next appears
-
-# the position where the blocks appear
-leftPosition = (100, 100)
-rightPosition = (500, 100)
-
-# the number of Blocks for each player at the beginning and maximum
-initialNumberOfBlocks = 10
-maxNumberOfBlocks = 15
-
 PPM = 20.0 # number of pixels per meter
 TARGET_FPS = 60 # the targeted frames per second
 TIME_STEP = 1.0 / TARGET_FPS
+
+# ****************** bat config ******************
+maxBatSize = 10 # maximal bat size in meters XXX should be dependent on the actual resolution
+
+# ****************** wall config ******************
+brickSize = (50, 50) # the size of a brick in pixels XXX should be dependent on the actual resolution
+initialNumberOfBlocks = 10
+maxNumberOfBlocks = 30
+
+# ****************** bonus config ******************
+bonusTime = 3 # the time in seconds a bonus waits for interaction before disappearing
+
+
